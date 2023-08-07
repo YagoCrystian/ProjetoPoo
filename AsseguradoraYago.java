78214b09d85ce2e6643fea003fdad537ad93fb61
YagoCrystian <103502519+YagoCrystian@users.noreply.github.com>
2023-08-07 08:48:47 -0300

    Add files via upload

---
+++ AsseguradoraYago.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asseguradorayago;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class AsseguradoraYago {
+
+    /**
+     * @param args the command line arguments
+     */
+//TODAS AS NOVAS INSTANCIAS DE CÓDIGO VÃO AQUI!
+    public static void main(String[] args) {
+        //declarando cliente como instancia nova        
+        Cliente cliente = new Cliente("Yago Crystian", "111.222.333-45");
+        
+        //declarando seguro como instancia nova   
+        Seguro seguro = new Seguro("1500.00", SIM, )
+        
+        //declarando carro como instancia nova   
+        
+        
+        //declarando modelo como instancia nova   
+    }
+    
+}
